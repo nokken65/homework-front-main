6fe734b7abc81025edd2685a08f0d776ce9570c5
nokken65 <unknown65182@gmail.com>
2024-01-07 11:15:54 +0300

    feat: complete all HW

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,15 +1,15 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: VoidFunction;
+  name: string;
 };
 
 export const Button = (props: ButtonPropsType) => {
   const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
+    props.callBack();
   };
 
   return (
-    <button id={'hw04-button'} onClick={callBackHandler}>
+    <button id={"hw04-button"} onClick={callBackHandler}>
       {props.name}
     </button>
   );
